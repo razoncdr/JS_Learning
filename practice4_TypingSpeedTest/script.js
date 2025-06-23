e03d0e1e0bdba8791029508a1c6133ab3c986ca4
Rejwanul Haque <rejwanul7296@gmail.com>
2025-06-23 13:32:31 +0600

    feat(TypingSpeedTest): added auto focus in the input box

--- practice4_TypingSpeedTest/script.js
+++ practice4_TypingSpeedTest/script.js
@@ -20,41 +20,46 @@ let finished = false;
 
 input.addEventListener('paste', (e) => e.preventDefault());
 input.addEventListener('contextmenu', (e) => e.preventDefault());
+input.focus();
+console.log(document.activeElement);
+
+
+
 
 input.addEventListener('input', ()=>{
     if(!startTime) startTime = new Date();
     let result = document.querySelector("#result");
     if (!finished && input && input.value === curSentence) {
-    endTime = new Date();
-
-    let wordCount = curSentence.trim().split(/\s+/).length;
-
-    let duration = (endTime - startTime) / 1000; // in seconds
-    let speed = curSentence.length / duration;   // characters per second
-    let speedWpm = (wordCount/duration)*60;
-    result.textContent = 
+        endTime = new Date();
+        
+        let wordCount = curSentence.trim().split(/\s+/).length;
+        
+        let duration = (endTime - startTime) / 1000; // in seconds
+        let speed = curSentence.length / duration;   // characters per second
+        let speedWpm = (wordCount/duration)*60;
+        result.textContent = 
         `✅ Typing Complete!\n` +
         `⏱️ Time Taken: ${duration.toFixed(2)} seconds\n` +
         `⌨️ Speed: ${speed.toFixed(2)} CPS (Characters Per Second)` +
         '\n'+
         `📝 Speed: ${speedWpm.toFixed(2)} WPM (Words Per Minute)`;
-
-    result.style.whiteSpace = "pre-line"; // So \n works in the browser
-    result.style.color = "green";
-    input.disabled = true;
-    finished = true;
-}
-// else if(!finished){
-//         result.textContent = "Not Done";
-//         result.style.color = "red";
-//     }
-})
-
-
-
-
-let restartBtn = document.querySelector("#restart");
-restartBtn.addEventListener("click", ()=>{
+        
+        result.style.whiteSpace = "pre-line"; // So \n works in the browser
+        result.style.color = "green";
+        input.disabled = true;
+        finished = true;
+    }
+    // else if(!finished){
+        //         result.textContent = "Not Done";
+        //         result.style.color = "red";
+        //     }
+    })
+    
+    
+    
+    
+    let restartBtn = document.querySelector("#restart");
+    restartBtn.addEventListener("click", ()=>{
     startTime = null;
     endTime = null;
     result.textContent="";
